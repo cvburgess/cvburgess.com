201243fde5746b5f33a0be9cf4fc62723bccbdde
Charles Burgess <consulting@cvburgess.com>
2020-07-22 22:31:32 -0400

    add header

--- src/components/header.js
+++ src/components/header.js
@@ -1,35 +1,85 @@
-import { Link } from "gatsby";
-import PropTypes from "prop-types";
 import React from "react";
+import PropTypes from "prop-types";
+import { graphql, Link, useStaticQuery } from "gatsby";
+import Img from "gatsby-image";
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      file(relativePath: { eq: "charles.png" }) {
+        childImageSharp {
+          # Specify the image processing specifications right in the query.
+          # Makes it trivial to update as your page's design changes.
+          fixed(height: 450) {
+            ...GatsbyImageSharpFixed
+          }
+        }
+      }
+    }
+  `);
+
+  return (
+    <header
       style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        background: `#363537`,
+        borderBottom: `30px solid #FF9C28`,
+        height: "586px",
+        display: "flex",
+          marginBottom: `125px`,
       }}
     >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+      <div
+        style={{
+          width: "50%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-end",
+          marginBottom: `-125px`,
+        }}
+      >
+        <Img fixed={data.file.childImageSharp.fixed} />
+      </div>
+      <div
+        style={{
+          width: "50%",
+        }}
+      >
+        <h1
           style={{
-            color: `white`,
+            marginTop: "100px",
+            color: `#00C8B6`,
+            lineHeight: 1.4,
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-);
+          Hey there, I'm Charles
+        </h1>
+        <p
+          style={{
+            fontFamily: "Roboto-Regular",
+            fontSize: "22px",
+            color: "#FFFFFF",
+          }}
+        >
+          Welcome to your new Gatsby site.
+          <br />
+          Now go build something great.
+          <br />
+          Welcome to your new Gatsby site.
+        </p>
+        <p
+          style={{
+            fontFamily: "Roboto-Regular",
+            fontSize: "22px",
+            color: "#FFFFFF",
+          }}
+        >
+          Yoga. Technology. Teacher.
+        </p>
+      </div>
+    </header>
+  );
+};
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
--- src/components/seo.js
+++ src/components/seo.js
@@ -10,7 +10,7 @@ import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
-function SEO({ description, lang, meta, title }) {
+function SEO({ description = "", lang = "en", meta = [], title }) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -71,12 +71,6 @@ function SEO({ description, lang, meta, title }) {
     />
   );
 }
-
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-};
 
 SEO.propTypes = {
   description: PropTypes.string,
