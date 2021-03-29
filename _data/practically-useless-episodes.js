6869288503e1f877f5a1377401ffd2c8ea09cc96
Charles Burgess <consulting@cvburgess.com>
2021-03-28 20:29:02 -0400

    dynamically load transcripts

--- _data/practically-useless-episodes.js
+++ _data/practically-useless-episodes.js
@@ -1,5 +1,18 @@
 const Feed = require("rss-to-json");
+const fetch = require("node-fetch");
 
 const FEED_URL = "https://feeds.captivate.fm/practically-useless/";
 
-module.exports = () => Feed.load(FEED_URL).then((rss) => rss.items);
+const getTranscript = async (url) =>
+  url && fetch(url).then((res) => res.text());
+
+module.exports = async () => {
+  const rss = await Feed.load(FEED_URL);
+
+  return Promise.all(
+    rss.items.map(async (episode) => ({
+      ...episode,
+      transcript: await getTranscript(episode.podcast_transcript),
+    }))
+  );
+};
