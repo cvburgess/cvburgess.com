b958e562b3dd896a15bc670a34e29fb30da619fb
Charles Burgess <consulting@cvburgess.com>
2023-10-01 08:27:18 -0400

    Migratie to Lume (#17)
    * Configure Lume
    
    * Update layout references
    
    * Rename files to remove _
    
    * Fix 404 page
    
    * Fix note blocks
    
    * Update card.njk

---
+++ _config.ts
@@ -0,0 +1,83 @@
+import lume from "lume/mod.ts";
+import { Data } from "lume/core.ts";
+
+import anchor from "npm:markdown-it-anchor";
+import { container } from "npm:@mdit/plugin-container";
+
+const BASE_URL = "https://cvburgess.com";
+const PRIMARY_COLOR = "#ffbc51";
+
+const site = lume({ location: new URL(BASE_URL), src: "./src" });
+
+// --------- PREPROCESS FILES ---------- //
+
+site.preprocess("*", (page) => {
+  page.data.inputPath = page.src.path + page.src.ext;
+});
+
+// --------- MARKDOWN PLUGINS ---------- //
+
+site.hooks.addMarkdownItPlugin(anchor, {
+  level: 2,
+  permalink: anchor.permalink.headerLink(),
+});
+
+site.hooks.addMarkdownItPlugin(container, {
+  name: "note",
+  openRender: () =>
+    `<div class="callout"><p class="callout-title">HEADS UP!</p>`,
+});
+
+// --------- PUBLIC FILES ---------- //
+
+site.copyRemainingFiles();
+
+// --------- CUSTOM FILE LOADERS ---------- //
+
+async function svgLoader(path: string): Promise<Data> {
+  let content = await Deno.readTextFile(path);
+  content = content.replace(/--primary/gi, PRIMARY_COLOR);
+  return { content };
+}
+
+site.loadAssets([".svg"], svgLoader);
+
+// --------- FILTERS ---------- //
+
+const makeAbsoluteUrl = (path: string) => `${BASE_URL}${path}`;
+
+site.filter("absoluteUrl", makeAbsoluteUrl);
+
+site.filter(
+  "og",
+  (image = "default") => makeAbsoluteUrl(`/img/og/og-${image}.jpg`),
+);
+
+site.filter("hostname", (url: string) => new URL(url).hostname);
+
+site.filter("localDate", (value: string) => {
+  const date = new Date(value);
+  return date.toLocaleDateString("en-US", {
+    month: "long",
+    day: "2-digit",
+    year: "numeric",
+  });
+});
+
+site.filter("episodeNumber", (value = 0) => {
+  const leadingZeros = "0".repeat(3 - value.toString().length);
+  return `${leadingZeros}${value}`;
+});
+
+site.helper("button", (text, link, classes) => {
+  // const isInternal = link?.startsWith("/");
+  // const arrow = isInternal ? "→" : "↗";
+  const target = link?.startsWith("/")
+    ? `target="_self"`
+    : `target="_blank" rel="noopener"`;
+  return `<div class="button ${
+    classes || ""
+  }"><a href="${link}" ${target}><span>${text}</span></a></div>`;
+}, { type: "tag" });
+
+export default site;
