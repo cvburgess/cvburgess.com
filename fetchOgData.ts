eb3e4fa941b40ab6c043dec748dcab2a90384d4d
Charles Burgess <consulting@cvburgess.com>
2023-10-02 22:34:11 -0400

    Add script for getting OG data

---
+++ fetchOgData.ts
@@ -0,0 +1,34 @@
+import { DOMParser } from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
+
+interface OGData {
+  title: string;
+  subtitle: string;
+  image: string;
+  hostname: string;
+}
+
+export const fetchOgData = async (url: string): Promise<OGData> => {
+  const r = await fetch(url);
+  const html = await r.text();
+
+  const doc = new DOMParser().parseFromString(html, "text/html")!;
+
+  const selectors = {
+    title: "meta[property='og:title']",
+    subtitle: "meta[property='og:description']",
+    image: "meta[property='og:image']",
+  };
+
+  const getData = (prop: string) => {
+    const node = doc.querySelector(prop);
+    return node?.getAttribute("content") ?? "";
+  };
+
+  const data = Object.fromEntries(
+    Object.entries(selectors).map((
+      [key, selector],
+    ) => [key, getData(selector)]),
+  );
+
+  return { ...data, hostname: new URL(url).hostname };
+};
